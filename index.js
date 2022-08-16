afcfa1349c6c011b1e7b9a9be155dacd4d389e4b
wilfred <wilfredkorir46@gmail.com>
2022-08-16 11:27:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  
+  let result;
+  if(distance <= 400){
+    result = `This one is on me!`
+  }else if(distance >= 400 && distance <= 2500){
+    result = `I will gladly take your thirty bucks.`
+  }else if (distance > 2500){
+    result = `No can do.`
+  }
+  return result;
 }
+console.log(scuberGreetingForFeet(199));
+console.log(scuberGreetingForFeet(2501));
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  return city === `NYC` ? "Ok, sounds good." : "No go.";
+
+
 }
+console.log(ternaryCheckCity(`NYC`));
+console.log(ternaryCheckCity(`Pittsburgh`));
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+         case `generous`:
+         return `Thank you so much.`
+  }
+  switch(tip){
+    case `not as generous`:
+      return `Thank you.`
+      default:{
+        return `Bye.`
+  }
+}
 }
